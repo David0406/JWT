323939076d7b7635cdec92baa1d6f57c4e4f6203
David <jianjunwei0406@gmail.com>
2021-10-12 11:51:05 +0800

    加入專案檔案。

---
+++ JWT/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using JWT.Models;
+using JWT.Repositories;
+using JWT.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult<dynamic> Authenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            if (user == null)
+                return NotFound(new { message = "User or password invalid" });
+
+            var token = TokenService.CreateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+            
+        }
+
+        [HttpGet]
+        [Authorize]
+        public string Anonymous()
+        {
+            return "You are Anonymous";
+        }
+        [HttpGet]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Manager";
+
+        [HttpGet]
+        [AllowAnonymous]
+        public string Home() => "normal";
+    }
+}
---
+++ JWT/Services/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT.Services
+{
+    public static class Settings
+    {
+        public static string Secret = "marcy9d8534b48w951b9287d492b256x";
+        public static string Issuer = "JWT";
+        public static string Audience = "JWTmanager";
+    }
+}
